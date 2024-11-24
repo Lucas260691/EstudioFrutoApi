f7ba9440a86e87998acfced56b902c5b7caae9d3
Lucas Sodre <lucasmeinen@yahoo.com.br>
2024-11-24 13:20:51 -0300

    Finalizando controlers

--- EstudioFrutoApi/Controllers/SalasController.cs
+++ EstudioFrutoApi/Controllers/SalasController.cs
@@ -1,6 +1,102 @@
-﻿namespace EstudioFrutoApi.Controllers
+﻿using EstudioFrutoApi.Data;
+using EstudioFrutoApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EstudioFrutoApi.Controllers
 {
-    public class SalasController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SalasController : ControllerBase
     {
+        private readonly AgendaContext _context;
+
+        public SalasController(AgendaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Salas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Sala>>> GetSalas()
+        {
+            return await _context.Salas.ToListAsync();
+        }
+
+        // GET: api/Salas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Sala>> GetSala(int id)
+        {
+            var sala = await _context.Salas.FindAsync(id);
+
+            if (sala == null)
+            {
+                return NotFound();
+            }
+
+            return sala;
+        }
+
+        // POST: api/Salas
+        [HttpPost]
+        public async Task<ActionResult<Sala>> PostSala(Sala sala)
+        {
+            _context.Salas.Add(sala);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetSala), new { id = sala.SalaID }, sala);
+        }
+
+        // PUT: api/Salas/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSala(int id, Sala sala)
+        {
+            if (id != sala.SalaID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(sala).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SalaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Salas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSala(int id)
+        {
+            var sala = await _context.Salas.FindAsync(id);
+            if (sala == null)
+            {
+                return NotFound();
+            }
+
+            _context.Salas.Remove(sala);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SalaExists(int id)
+        {
+            return _context.Salas.Any(e => e.SalaID == id);
+        }
+
     }
 }
