eb927ddfd9eca6468ad60fac97262452aba40f11
Lucas Sodre <lucasmeinen@yahoo.com.br>
2024-11-23 23:17:00 -0300

    Criando migrations e ajustando config do appsetings

---
+++ EstudioFrutoApi/Migrations/20241124004728_InitialCreate.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EstudioFrutoApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Alunos",
+                columns: table => new
+                {
+                    AlunoID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nivel = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Contato = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alunos", x => x.AlunoID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Instrutores",
+                columns: table => new
+                {
+                    InstrutorID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DisponibilidadeHoraria = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Contato = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Instrutores", x => x.InstrutorID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Salas",
+                columns: table => new
+                {
+                    SalaID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Capacidade = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Salas", x => x.SalaID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Horarios",
+                columns: table => new
+                {
+                    HorarioID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DataHora = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SalaID = table.Column<int>(type: "int", nullable: false),
+                    InstrutorID = table.Column<int>(type: "int", nullable: false),
+                    AlunoID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Horarios", x => x.HorarioID);
+                    table.ForeignKey(
+                        name: "FK_Horarios_Alunos_AlunoID",
+                        column: x => x.AlunoID,
+                        principalTable: "Alunos",
+                        principalColumn: "AlunoID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Horarios_Instrutores_InstrutorID",
+                        column: x => x.InstrutorID,
+                        principalTable: "Instrutores",
+                        principalColumn: "InstrutorID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Horarios_Salas_SalaID",
+                        column: x => x.SalaID,
+                        principalTable: "Salas",
+                        principalColumn: "SalaID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Horarios_AlunoID",
+                table: "Horarios",
+                column: "AlunoID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Horarios_InstrutorID",
+                table: "Horarios",
+                column: "InstrutorID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Horarios_SalaID",
+                table: "Horarios",
+                column: "SalaID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Horarios");
+
+            migrationBuilder.DropTable(
+                name: "Alunos");
+
+            migrationBuilder.DropTable(
+                name: "Instrutores");
+
+            migrationBuilder.DropTable(
+                name: "Salas");
+        }
+    }
+}
