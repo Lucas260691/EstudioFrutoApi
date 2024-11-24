cb17ba3426b9daa005dbdff94359a9d934a0fc38
Lucas Sodre <lucasmeinen@yahoo.com.br>
2024-11-24 00:24:39 -0300

    Criando controllers

---
+++ EstudioFrutoApi/Controllers/AlunosController.cs
@@ -0,0 +1,103 @@
+﻿using EstudioFrutoApi.Data;
+using EstudioFrutoApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EstudioFrutoApi.Controllers
+{
+    
+        [Route("api/[controller]")]
+        [ApiController]
+        public class AlunosController : ControllerBase
+        {
+            private readonly AgendaContext _context;
+
+            public AlunosController(AgendaContext context)
+            {
+                _context = context;
+            }
+
+            // GET: api/Alunos
+            [HttpGet]
+            public async Task<ActionResult<IEnumerable<Aluno>>> GetAlunos()
+            {
+                return await _context.Alunos.ToListAsync();
+            }
+
+            // GET: api/Alunos/5
+            [HttpGet("{id}")]
+            public async Task<ActionResult<Aluno>> GetAluno(int id)
+            {
+                var aluno = await _context.Alunos.FindAsync(id);
+
+                if (aluno == null)
+                {
+                    return NotFound();
+                }
+
+                return aluno;
+            }
+
+            // POST: api/Alunos
+            [HttpPost]
+            public async Task<ActionResult<Aluno>> PostAluno(Aluno aluno)
+            {
+                _context.Alunos.Add(aluno);
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction(nameof(GetAluno), new { id = aluno.AlunoID }, aluno);
+            }
+
+            // PUT: api/Alunos/5
+            [HttpPut("{id}")]
+            public async Task<IActionResult> PutAluno(int id, Aluno aluno)
+            {
+                if (id != aluno.AlunoID)
+                {
+                    return BadRequest();
+                }
+
+                _context.Entry(aluno).State = EntityState.Modified;
+
+                try
+                {
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AlunoExists(id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+                return NoContent();
+            }
+
+            // DELETE: api/Alunos/5
+            [HttpDelete("{id}")]
+            public async Task<IActionResult> DeleteAluno(int id)
+            {
+                var aluno = await _context.Alunos.FindAsync(id);
+                if (aluno == null)
+                {
+                    return NotFound();
+                }
+
+                _context.Alunos.Remove(aluno);
+                await _context.SaveChangesAsync();
+
+                return NoContent();
+            }
+
+            private bool AlunoExists(int id)
+            {
+                return _context.Alunos.Any(e => e.AlunoID == id);
+            }
+        
+    }
+}
---
+++ EstudioFrutoApi/Controllers/InstrutoresController.cs
@@ -0,0 +1,99 @@
+﻿using EstudioFrutoApi.Data;
+using EstudioFrutoApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EstudioFrutoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InstrutoresController : ControllerBase
+    {
+        private readonly AgendaContext _context;
+
+        public InstrutoresController(AgendaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Instrutor>>> GetInstrutores()
+        {
+            return await _context.Instrutores.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Instrutor>> GetInstrutor(int id)
+        {
+            var instrutor = await _context.Instrutores.FindAsync(id);
+
+            if (instrutor == null)
+            {
+                return NotFound();
+            }
+
+            return instrutor;
+        }
+
+        // POST: api/Instrutores
+        [HttpPost]
+        public async Task<ActionResult<Instrutor>> PostInstrutor(Instrutor instrutor)
+        {
+            _context.Instrutores.Add(instrutor);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetInstrutor), new { id = instrutor.InstrutorID }, instrutor);
+        }
+
+        // PUT: api/Instrutores/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutInstrutor(int id, Instrutor instrutor)
+        {
+            if (id != instrutor.InstrutorID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(instrutor).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!InstrutorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Instrutores/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteInstrutor(int id)
+        {
+            var instrutor = await _context.Instrutores.FindAsync(id);
+            if (instrutor == null)
+            {
+                return NotFound();
+            }
+
+            _context.Instrutores.Remove(instrutor);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool InstrutorExists(int id)
+        {
+            return _context.Instrutores.Any(e => e.InstrutorID == id);
+        }
+    }
+}
